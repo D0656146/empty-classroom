2507213dcd4fe258f0ff6b67991b286985b3f850
Jibanyan-FCU <d0745587@mail.fcu.edu.tw>
2020-06-03 14:15:41 +0800

    測試搜尋結果腳本

--- app/static/js/formRelate.js
+++ app/static/js/formRelate.js
@@ -66,19 +66,20 @@ function clearResult(){
 }
 
 function showResult(obj){
+    console.log(obj)
     $("#searchResult").append(
         `
             <div class="col-lg-3 col-md-4 col-sm-12 col-12 buildingImage">
                 <img src="../static/img/test.png" class="img-thumbnail" alt="photo">
             </div> 
             <div class="col-lg-7 col-md-6 col-sm-12 col-12 roomInformation">
-                <h5> ${obj.locate.building}${obj.locate.room} </h5>
+                <h5> ${obj["building"]}${obj["room"]} </h5>
                 <p> 插座數：4，位於教室後方 </p>
                 <p> 冷氣狀態：有節能管控 </p>
                 <small>史英間驗說，越海斯由了外一濟格科有升司。自對何話大利文此的力跑求想，事放獎兒理面境改的過一、任動商知此國少機合教產情你傳照些真負後在是國寶中上，同不考視人功利！我思工這，期一常的率人北今不流樹是！她紀先頭令日什去是至不力願樣：花多商造得都這候，術求麼響不覺。</small>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-12 col-12 roomTable">
-                <button class="btn btn-info" id="${obj.locate.building}${obj.locate.room}Building"> 教室課表 </button>
+                <button class="btn btn-info" id="${obj["building"]}${obj["room"]}Building"> 教室課表 </button>
             </div>
         `
     )
@@ -87,7 +88,7 @@ function showResult(obj){
 $("document").ready(function () {
 
     // no-used reset form
-    clearResult()
+    //clearResult()
     $("#resetBtn").click(function () {
         $("#room").val("")
         alert('這個重設按鈕目前沒啥屁用，只是擺好看的')
@@ -108,33 +109,13 @@ $("document").ready(function () {
             type: "GET",
             success: function(e){
                 alert("success section")
-                var x = {
-                    "searchMode": 0,
-                    "locate":[
-                        {
-                            "building": "資電",
-                            "room": 404,
-                            "info": ""
-                        },
-                        {
-                            "building": "資電",
-                            "room": 405,
-                            "info": ""
-                        },
-                        {
-                            "building": "資電",
-                            "room": 406,
-                            "info": ""
-                        },
-                    ]
-                }
-                console.log(x)
-                if(x.lenght == 0){
+                console.log(e)
+                if(e.lenght == 0){
                     $("noResult").show()
                 }
                 else{
-                    for(var i = 0; i < x.lenght; i++){
-                        showResult(x[i])
+                    for(var i = 0; i < e.lenght; i++){
+                        showResult(e[i])
                     }
                 }
             },
@@ -143,4 +124,36 @@ $("document").ready(function () {
             }
         })
     })
+
+    $('#testOutput').click(function(){
+        var x = {
+            "searchMode": 0,
+            "locate":[
+                {
+                    "building": "資電",
+                    "room": 504,
+                    "info": ""
+                },
+                {
+                    "building": "資電",
+                    "room": 405,
+                    "info": ""
+                },
+                {
+                    "building": "資電",
+                    "room": 406,
+                    "info": ""
+                }
+            ] 
+        }
+        for(var i = 0; i < x["locate"].length; i++){
+            console.log(x["locate"][i])
+            showResult(x["locate"][i])
+            
+        }
+    })
 })
+
+
+
+
--- app/static/js/searchServer.js
+++
@@ -1,5 +0,0 @@
-var http = require('http')
-
-var server = http.createServer(function(req, res){
-    console.log(req)
-}).listen(8090)
