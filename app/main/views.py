0ea53766cf7cf3d8f4daf354abb90720f6c42cec
D0656146 <d0656146@mail.fcu.edu.tw>
2020-06-08 17:13:45 +0800

    完成資料轉接

--- app/main/views.py
+++ app/main/views.py
@@ -10,28 +10,32 @@ def root():
 
 @main.route('/search/')
 def search():
+    searchMode = request.args.get('searchMode')
     building = request.args.get('building')
-    if building:
-        room = request.args.get('room')
-        if room:
-            return search_curriculum(building, room)
-        else:
-            dayOfWeek = request.args.get('dayOfWeek')
-            startAt = request.args.get('startAt')
-            endAt = request.args.get('endAt')
-            if not startAt or not endAt:
-                return 'leak of arguments'
-            sessions = range((dayOfWeek - 1) * 7 + startAt,
-                             (dayOfWeek - 1) * 7 + endAt + 1)
-            return search_empty_classroom(building, sessions)
-    else:
+    room = request.args.get('room')
+    if searchMode == 0:
+        dayOfWeek = request.args.get('dayOfWeek')
+        startAt = request.args.get('startAt')
+        endAt = request.args.get('endAt')
+        if not building or not startAt or not endAt or startAt < endAt:
+            return 'leak of arguments'
+        sessions = range((dayOfWeek - 1) * 7 + startAt,
+                         (dayOfWeek - 1) * 7 + endAt + 1)
+        return search_empty_classroom(building, room, sessions)
+    elif searchMode == 2:
+        if not building or not room:
+            return 'unknown classroom'
+        return search_curriculum(building, room)
+    elif searchMode is None:
         return render_template('mainPage.html')
+    else:
+        return 'unknown search mode'
 
 
 def search_curriculum(building, room):
     db = get_db()
     cursor = db.execute(
-        'SELECT session, course FROM Period WHERE building = ? AND room = ? ORDER BY session', (building, room))
+        'SELECT session, course FROM Period WHERE building = ? AND room = ? ORDER BY session;', (building, room))
     period_fields = ('session', 'course')
     timetable = []
     for period in cursor:
@@ -44,32 +48,52 @@ def search_curriculum(building, room):
     return curriculum
 
 
-def search_empty_classroom(building, sessions):
+def search_empty_classroom(building, room, sessions):
     db = get_db()
-    cursor = db.execute(
-        'SELECT session, course FROM Period WHERE building = ? AND room = ? ORDER BY session', (building, room))
-    return 'not yet done'
+    command = 'SELECT id, socket, ac FROM Classroom WHERE building = ${building}'
+    if room:
+        command += ' AND room = ${room}'
+    command += ' AND id NOT EXISTS (SELECT classroom FROM Period WHERE'
+    for session in sessions:
+        command += ' session = ${session} OR'
+    command = command[0:-2]
+    command += ') ORDER BY id;'
+    cursor = db.execute(command)
+    classroom_field = ('room', 'socket', 'ac')
+    classrooms = []
+    for classroom in cursor:
+        json_classroom = {}
+        for index, field in enumerate(classroom):
+            json_classroom[classroom_field[index]] = field
+        classrooms.append(copy.deepcopy(json_classroom))
+    empty_classrooms = {'building': building, 'classrooms': classrooms}
+    return empty_classrooms
 
 
 @main.route('/board/')
 def board():
     db = get_db()
-    page = request.args.get('page')
-    if page:
-        cursor = db.execute(
-            'SELECT * FROM Post ORDER BY isPinned DESC, posttime DESC')
-        post_fields = ('id', 'title', 'content',
-                       'posttime', 'isPinned', 'username')
-        posts = []
-        for index, post in enumerate(cursor):
-            if index >= page * 10:
-                break
-            if index < (page - 1) * 10:
-                continue
-            json_post = {}
-            for index, field in enumerate(post):
-                json_post[post_fields[index]] = field
-            posts.append(copy.deepcopy(json_post))
-        return posts
+    if request.method == 'POST':
+        db.execute('INSERT INTO table_name (title, content, username) VALUES (?, ?, ?);',
+                   (request.values[title], request.values[content], request.values[username]))
+        return redirect("/board/")
     else:
-        return render_template('messageBoard.html')
+        page = request.args.get('page')
+        if page:
+            cursor = db.execute(
+                'SELECT * FROM Post ORDER BY isPinned DESC, posttime DESC;')
+            post_fields = ('id', 'title', 'content',
+                           'posttime', 'isPinned', 'username')
+            posts = []
+            for index, post in enumerate(cursor):
+                if index >= page * 10:
+                    break
+                if index < (page - 1) * 10:
+                    continue
+                json_post = {}
+                for index, field in enumerate(post):
+                    json_post[post_fields[index]] = field
+                posts.append(copy.deepcopy(json_post))
+            return posts
+        else:
+            return render_template('messageBoard.html')
